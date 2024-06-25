6d21161c678f637feaa5f7b442c3feaba43bfc2e
Whal3 <leehan416@gmail.com>
2024-06-25 11:49:11 +0900

    [upload] loging

---
+++ Namukkun/src/main/java/com/pard/namukkun/login/dto/KakaoLoginDto.java
@@ -0,0 +1,13 @@
+package com.pard.namukkun.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class KakaoLoginDto {
+    public String accessToken;
+    public String refreshToken;
+}
