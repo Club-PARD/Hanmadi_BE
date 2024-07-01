eb9b2db8cf9709e72e7aa34ee798e35a61e2181b
Whal3 <leehan416@gmail.com>
2024-07-01 15:49:25 +0900

    [upload] user delete, update
    유저 제거, 유저 수정 추가

---
+++ Namukkun/src/main/java/com/pard/namukkun/user/dto/UserDeleteDTO.java
@@ -0,0 +1,11 @@
+package com.pard.namukkun.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDeleteDTO {
+    Long id;
+
+}
--- Namukkun/src/main/java/com/pard/namukkun/user/entity/User.java
+++ Namukkun/src/main/java/com/pard/namukkun/user/entity/User.java
@@ -2,43 +2,48 @@ package com.pard.namukkun.user.entity;
 
 import com.pard.namukkun.post.entity.Post;
 import com.pard.namukkun.user.dto.UserCreateDTO;
+import com.pard.namukkun.user.dto.UserUpdateDTO;
 import jakarta.persistence.*;
 import lombok.*;
 import net.minidev.json.annotate.JsonIgnore;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long userId;
-
+    private String kakaoId;
+    private String nickName;
     private String email;
-    private Long authId;
-    private String name;
 
-//    private // 유저가 쓴 글
+    private Integer local;
 
-    public User(User user) {
-        this.authId = user.getAuthId();
-        this.email = user.getEmail();
-        this.name = user.getName();
-    }
+    private String profileImage; // kakao profile image
+
+
+//    private // TODO 유저가 쓴 글 저장
+//
+//    public User(User user) {
+//        this.authId = user.getAuthId();
+//        this.email = user.getEmail();
+//        this.nickName = user.getNickName();
+//    }
 
     public static User toEntity(UserCreateDTO userCreateDTO) {
         return User.builder()
-                .name(userCreateDTO.getName())
+                .nickName(userCreateDTO.getNickName())
                 .email(userCreateDTO.getEmail())
-                .authId(userCreateDTO.getAuthId())
                 .build();
     }
 
+    public void updateNickName(String nickName) {
+        this.nickName = nickName;
+    }
 }
