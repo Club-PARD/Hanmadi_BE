604486c1893b9cea38818c84a28f259076ca37d2
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2024-07-02 19:46:50 +0900

    [Fix] fix CorsConfig

--- Namukkun/src/main/java/com/pard/namukkun/config/CorsConfig.java
+++ Namukkun/src/main/java/com/pard/namukkun/config/CorsConfig.java
@@ -1,12 +1,9 @@
 package com.pard.namukkun.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -24,21 +21,8 @@ public class CorsConfig {
         config.addAllowedHeader("*"); //모든 header에 응답을 허용하겠다
         config.addAllowedMethod("*"); //모든 post,get,put,delete,patch 요청을 허용하겠다
         // 모든 메서드에 대한접근을 허용함
-        sourse.registerCorsConfiguration("*",config); //api로 들어오는 모든 요청은 이 config를 따르겠다
+        sourse.registerCorsConfiguration("*", config); //api로 들어오는 모든 요청은 이 config를 따르겠다
         // 위에서 만든 규칙들을 sourse에 적용시켜줌
         return new CorsFilter(sourse);
-
-
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST")
-                .allowedHeaders("*")
-                .allowCredentials(true);
     }
 }
