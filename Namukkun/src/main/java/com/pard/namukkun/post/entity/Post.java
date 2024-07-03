92e7e48939ff19dc6824b2a20ffd0e28ddba7d70
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2024-07-04 03:57:22 +0900

    [Delete] delete attachmentController.java

--- Namukkun/src/main/java/com/pard/namukkun/post/entity/Post.java
+++ Namukkun/src/main/java/com/pard/namukkun/post/entity/Post.java
@@ -1,12 +1,18 @@
 package com.pard.namukkun.post.entity;
 
+import com.nimbusds.openid.connect.sdk.assurance.evidences.attachment.Attachment;
 import com.pard.namukkun.Data;
+import com.pard.namukkun.attachment.entity.S3Attachment;
+import com.pard.namukkun.attachment.service.S3AttachmentService;
 import com.pard.namukkun.post.dto.PostCreateDTO;
 import com.pard.namukkun.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Entity
 @Getter
@@ -22,41 +28,44 @@ public class Post {
     private String title; // 제목
     private Integer postRegion; // 지역
     private Integer upCountPost; // 추천수
-    private String problem; //문제정의
-    private String solution; //해결방법
+    private String proBackground; // 제안배경
+    private String solution; // 해결방
     private String benefit; // 기대효과
     private boolean isDone; // 제출완료 (작성 후 7일이 지난거)
     private String postTime; // 작성된 날짜 및 시간
 
-
-    // 첨부파일 넣는것도 추가해야함
     @JoinColumn(nullable = false, name = "user_Id")
     @ManyToOne
     private User user;
 
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<S3Attachment> s3Attachments = new ArrayList<>();
 
-    public Post(String title, Integer region, Integer upCountPost,
-                String problem, String solution, String benefit) {
-        this.title = title;
-        this.postRegion = region;
-        this.upCountPost = upCountPost;
-        this.problem = problem;
-        this.solution = solution;
-        this.benefit = benefit;
-        this.isDone = false;
+    public void addS3Attachment(String fileUrl) {
+
+        if(this.s3Attachments == null) {
+            this.s3Attachments = new ArrayList<>();
+        }
+        S3Attachment s3Attachment = new S3Attachment();
+        s3Attachment.setFileUrl(fileUrl);
+        s3Attachment.setPost(this);
+        this.s3Attachments.add(s3Attachment);
     }
 
     public static Post toEntity(PostCreateDTO postCreateDTO, User user) {
-        return Post.builder()
+        Post post = Post.builder()
                 .title(postCreateDTO.getTitle())
                 .postRegion(postCreateDTO.getPostRegion())
                 .upCountPost(postCreateDTO.getUpCountPost())
-                .problem(postCreateDTO.getProblem())
+                .proBackground(postCreateDTO.getProBackground())
                 .solution(postCreateDTO.getSolution())
                 .benefit(postCreateDTO.getBenefit())
                 .postTime(Data.getNowDate())
                 .isDone(false)
                 .user(user)
                 .build();
+        return post;
     }
+
+
 }
