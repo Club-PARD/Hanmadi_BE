cb59f35708854e68d60b7562bcdfc86c684503b5
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2024-07-04 16:20:52 +0900

    [Fix] httpsController

--- Namukkun/src/main/java/com/pard/namukkun/httpsController.java
+++ Namukkun/src/main/java/com/pard/namukkun/httpsController.java
@@ -1,22 +1,27 @@
 package com.pard.namukkun;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpResponse;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/")
 public class httpsController {
     @GetMapping("")
-    public Integer get() {
-        return 200;
+    public ResponseEntity<?> get(){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("")
-    public Integer post() {
-        return 200;
+    public ResponseEntity<?> post(){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
