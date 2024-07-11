9a500709d3128f7d9d957e6e021f6eedd74bea53
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2024-07-11 17:42:03 +0900

    [Fix] fix log format

--- Namukkun/src/main/java/com/pard/namukkun/post/service/PostService.java
+++ Namukkun/src/main/java/com/pard/namukkun/post/service/PostService.java
@@ -161,7 +161,7 @@ public class PostService {
                         log.info("프로트에서 받은 이미지 이름임: {}", decodedPostImgName);
                         log.info("유저에 있는 이미지 주소임: {}", img.getImgUrl());
                         String decodedImgUrl = URLDecoder.decode(img.getImgUrl(), StandardCharsets.UTF_8);
-                        log.info("유저에 있는 이미지 주소 디코딩한거임: {}", decodedImgUrl);
+                        log.info("유저에 있는 이미지 주소 디코딩한거임: {}", URLDecoder.decode(decodedImgUrl, StandardCharsets.UTF_8));
                         if (decodedImgUrl.contains(decodedPostImgName)) {
                             sb.append("[이미지: ").append(img.getImgUrl()).append("]");
                             imgsToRemove.add(img);
